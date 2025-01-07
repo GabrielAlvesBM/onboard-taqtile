1ee081646ba90131a2b7b93de756faa2d102d60d
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-07 19:20:06 -0300

    feat: title in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import Button from '../components/Button';
 const Login = () => {
   return (
     <main>
+      <h1 className='login-title'>Bem-vindo(a) à Taqtile!</h1>
       <form className='login-form' action='#'>
         <Input type='text' placeholder='Email' />
         <Input type='text' placeholder='Senha' />
