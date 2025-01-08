65a53672c6d3046958c48f36bfa6f2b40836547e
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-08 11:02:37 -0300

    style: remove the BEM methodology

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,16 +6,16 @@ const Login = () => {
       <h1 className='login-title'>Bem-vindo(a) à Taqtile!</h1>
       <form className='login-form' action='#'>
         <input
-          className='login__input'
+          className='login-input'
           type='email'
           placeholder='Email'
         />
         <input
-          className='login__input'
+          className='login-input'
           type='password'
           placeholder='Senha'
         />
-        <Button className='login__submit-button' text='Entrar' />
+        <Button className='login-submit-button' text='Entrar' />
       </form>
     </main>
   );
