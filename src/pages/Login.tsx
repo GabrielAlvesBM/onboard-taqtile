632029953e2f8581a1427eaf26e3b97ac4e41008
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-09 11:27:39 -0300

    refactor: implement controlled components in inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,13 +4,19 @@ import ErrorMsgs from '../components/ErrorMsgs';
 
 const Login = () => {
   const [errorMsgs, setErrorMsgs] = useState<string[] | null>(null);
+  const [emailInput, setEmailInput] = useState<string>('');
+  const [passwordInput, setPasswordInput] = useState<string>('');
+
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmailInput(event.target.value);
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPasswordInput(event.target.value);
+  }
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-    const form = event.target as HTMLFormElement;
-
-    const emailInput = form.querySelector('#email') as HTMLInputElement;
-    const passwordInput = form.querySelector('#password') as HTMLInputElement;
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).+$/;
@@ -19,15 +25,15 @@ const Login = () => {
 
     let newErrorMsgs: string[] = [];
 
-    if (!emailInput.value) {
+    if (!emailInput) {
       newErrorMsgs.push('Insira um Email.');
-    } else if (!emailRegex.test(emailInput.value)) {
+    } else if (!emailRegex.test(emailInput)) {
       newErrorMsgs.push("Email inválido. O formato correto é 'usuario@dominio.com'.");
     }
 
-    if (passwordInput.value.length < 7) {
+    if (passwordInput.length < 7) {
       newErrorMsgs.push('Senha inválida. Deve conter pelo menos 7 caracteres.');
-    } else if (!passwordRegex.test(passwordInput.value)) {
+    } else if (!passwordRegex.test(passwordInput)) {
       newErrorMsgs.push('Senha inválida. Deve conter pelo menos 1 letra e 1 número.');
     }
 
@@ -44,8 +50,24 @@ const Login = () => {
       <form className='login-form' action='#' onSubmit={handleSubmit}>
         <ErrorMsgs errorMsgs={errorMsgs} />
 
-        <input className='login-input' id='email' type='text' placeholder='Email' autoComplete='off' />
-        <input className='login-input' id='password' type='password' placeholder='Senha' autoComplete='off' />
+        <input
+          className='login-input'
+          id='email'
+          type='text'
+          placeholder='Email'
+          autoComplete='off'
+          value={emailInput}
+          onChange={handleEmailChange}
+        />
+        <input
+          className='login-input'
+          id='password'
+          type='password'
+          placeholder='Senha'
+          autoComplete='off'
+          value={passwordInput}
+          onChange={handlePasswordChange}
+        />
 
         <Button className='login-submit-button' text='Entrar' />
       </form>
