7aebd80aaf69347938dca37187d6fa6b82a74a8d
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-10 11:57:26 -0300

    feat: loading indicator for button and page

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner: React.FC = () => {
+  return (
+    <div className='spinner-page-container'>
+      <div className='spinner-page'></div>
+    </div>
+  );
+};
+
+export default Spinner;
