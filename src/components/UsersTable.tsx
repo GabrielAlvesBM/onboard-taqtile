7d2a0ab442c3dbb53224401ebb7ee99dc3b9d3eb
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-14 13:57:54 -0300

    refactor: change the UsersList to UsersTable

---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,31 @@
+type User = {
+  name: string;
+  email: string;
+};
+
+type UserListProps = {
+  users: User[];
+};
+
+const UsersList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <table className='users-table'>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={index}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UsersList;
